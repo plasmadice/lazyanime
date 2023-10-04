702ba84e331fa47b12eb5c5873ca730fd1a10871
plasmadice <19928793+plasmadice@users.noreply.github.com>
2023-10-03 23:43:28 -0400

    Hide horizontal scrollbar on anime row on main page

--- app/components/Row.tsx
+++ app/components/Row.tsx
@@ -18,7 +18,7 @@ export function Row({
           className.length ? ` ${className}` : ""
         }`}
       >
-        <div className="flex pl-4 h-full overflow-x-scroll">
+        <div className="flex pl-4 h-full overflow-x-scroll no-scrollbar">
           {animes.map((item: Anime) => (
             <Card key={item.id} anime={item} />
           ))}
